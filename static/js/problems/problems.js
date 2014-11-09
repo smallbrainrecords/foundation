f1facb723a9bd3267461e2140d0def30521578ff
timothyclemans <timothy.clemans@gmail.com>
2014-11-08 18:26:39 -0800

    Update problems.js

--- static/js/problems/problems.js
+++ static/js/problems/problems.js
@@ -1,5 +1,12 @@
 function getProblemHtml(problemJson) {
-    var problemElements = ['status', 'notes', 'goals', 'todos', 'images', 'relationships', 'guidelines', 'history'];
+    var problemElements = ['Status', 'Notes', 'Goals', 'Todos', 'Images', 'Relationships', 'Guidelines', 'History'];
     var problemHtml = '';
+    problemElements.forEach(function(problemElement) {
+        problemHtml += '<h4>'+problemElement+'</h4>';
+        problemHtml += '<div>';
+        problemHtml += window["generate"+problemElement+"Html"](problemJson);
+        problemHtml += '</div>';
+    });
+    
     return problemHtml;
 }
