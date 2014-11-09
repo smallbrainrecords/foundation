76e9827256e157d1dc84f28933f20e26fef5d00c
timothyclemans <timothy.clemans@gmail.com>
2014-11-08 18:17:49 -0800

    Create problems.js

---
+++ static/js/problems/problems.js
@@ -0,0 +1,5 @@
+function getProblemHtml(problemJson) {
+    var problemElements = ['status', 'notes', 'goals', 'todos', 'images', 'relationships', 'guidelines', 'history'];
+    var problemHtml = '';
+    return problemHtml;
+}
