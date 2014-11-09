67ec68d973258bda307e4ff269c4e06d5e842e18
timothyclemans <timothy.clemans@gmail.com>
2014-11-08 19:06:56 -0800

    Update problem_status.js

--- static/js/problems/problem_status.js
+++ static/js/problems/problem_status.js
@@ -1,4 +1,19 @@
+String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
 function generateProblemStatusHtml(problemJson) {
     var problemStatusHtml = '';
+    var problemStatusItems = ["is_controlled", "is_authenticated", "is_active"];
+    problemStatusElements.forEach(function(problemStatusElement) {
+        if (problemJson[problemStatusElement] == true) {
+            var checked = ' checked ';
+        } else {
+            var checked = '';
+        }
+        var problemStatusElementDisplayName = problemStatusElement.replace("_", " ").capitalize();
+        problemStatusHtml += '<input type="checkbox" attr="'+problemStatusElement+'" target="problem" id="' +
+        problemJson['problem_id'] + '" ' + checked + '/> '+problemStatusElementDisplayName;
+    });
     return problemStatusHtml;
 }
