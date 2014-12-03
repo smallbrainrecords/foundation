92f52037b9ef412fd70888623ff8f5b4ce66939a
timothyclemans <timothy.clemans@gmail.com>
2014-12-03 14:02:53 -0800

    Update problem_todos.js

--- static/js/problems/problem_todos.js
+++ static/js/problems/problem_todos.js
@@ -1,4 +1,24 @@
 function generateProblemTodosHtml(problemJson) {
     var problemTodosHtml = '';
+    todos = '<ul id="todo_' + problemJson['problem_id'] + '">';
+    todos +=
+        '<li><strong>Add:</strong> <input type="text" id="todo_input_' +
+        problemJson['problem_id'] +
+        '" /><input type="button" value="Submit" parent="' +
+        problemJson['problem_id'] + '" target="todo_input_' +
+        problemJson['problem_id'] +
+        '" object_type="todo" class="submit_data" /></li>';
+    for (var j = 0; j < problem['todos'].length; j++) {
+        if (problemJson['todos'][j]['accomplished'] == true) {
+            checked = ' checked ';
+        } else {
+            checked = '';
+        }
+        todos +=
+            '<li><input type="checkbox" attr="todo" id="' +
+            problemJson['todos'][j]['id'] + '" ' + checked +
+            '/>' + problemJson['todos'][j]['todo'] + '</li>';
+    }
+    todos += '</ul>';
     return problemTodosHtml;
 }
