9851ff4f625cd100e48ebc8aa23e137081a814f8
timothyclemans <timothy.clemans@gmail.com>
2014-12-27 09:46:26 -0800

    Update settings.py

--- emr/settings.py
+++ emr/settings.py
@@ -75,7 +75,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = BASE_DIR
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -83,6 +83,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static')
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
