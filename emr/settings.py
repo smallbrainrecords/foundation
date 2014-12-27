81d42f944a10db0ba5c1e01714f9122804704e7e
timothyclemans <timothy.clemans@gmail.com>
2014-12-27 09:50:29 -0800

    Update settings.py

--- emr/settings.py
+++ emr/settings.py
@@ -83,7 +83,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
