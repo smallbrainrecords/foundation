bfc70778bef404a6f6b393344d7bf332d2c1a87f
timothyclemans <timothy.clemans@gmail.com>
2014-12-27 09:30:32 -0800

    Update views.py

--- emr/views.py
+++ emr/views.py
@@ -172,7 +172,7 @@ def view_patient(request, user_id):
         pass
     import os
     import re
-    context['problem_elements'] = [ re.search('problem_(?P<element>\w+)', f).group('element') for f in os.listdir('/root/core/static/js/problems/') if not f == 'problem_element_template.js' or f == 'problems.js']
+    context['problem_elements'] = [ re.search('problem_(?P<element>\w+)', f).group('element') for f in os.listdir('/root/core/static/js/problems/') if not (f == 'problem_element_template.js' or f == 'problems.js')]
     return render_to_response("patient.html", context)
 
 @login_required
