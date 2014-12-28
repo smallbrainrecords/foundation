fd7fda9bbce18026eb2ac4d65b19eacda163efd3
timothyclemans <timothy.clemans@gmail.com>
2014-12-28 09:33:45 -0800

    Update problems.js

--- static/js/problems/problems.js
+++ static/js/problems/problems.js
@@ -22,7 +22,7 @@ function generateProblemHeadingHtml(problemJson) {
 function generateProblemElementsHtml(problemJson) {
     var problemElementsHtml = '';
     //var problemElements = ['Status', 'Notes', 'Goals', 'Todos', 'Images', 'Relationships', 'Guidelines', 'History'];    
-    var problemElements = ['Status', 'Notes', 'Goals', 'Todos', 'Images', 'Guidelines', 'History'];    
+    var problemElements = ['History', 'Status', 'Notes', 'Goals', 'Todos', 'Images', 'Relationships', 'Guidelines'];    
     problemElements.forEach(function(problemElement) {
         problemElementsHtml += '<h4>' + problemElement + '</h4>';
         problemElementsHtml += '<div style="background:orange">';
