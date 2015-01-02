b5d02a171d154869db314272931d4d9f54e396d8
timothyclemans <timothy.clemans@gmail.com>
2015-01-01 16:53:08 -0800

    Update problem_goals.js

--- static/js/problems/problem_goals.js
+++ static/js/problems/problem_goals.js
@@ -74,7 +74,8 @@ function generateProblemGoalsHtml(problem) {
             ] + '">' + problem['goals'][j]['goal'] +
             '<input type="button" class="show_goal" value="Show goal" target="goal_notes_' +
             problem['goals'][j]['id'] + '" goal="' +
-            problem['goals'][j]['id'] + '" />' + notes + 'test</div></li>';
+            problem['goals'][j]['id'] + '" /></div></li>';
+            //problem['goals'][j]['id'] + '" />' + notes + 'test</div></li>';
     }
     problemGoalsHtml += 'another test</ul>big test';    
     return problemGoalsHtml;
