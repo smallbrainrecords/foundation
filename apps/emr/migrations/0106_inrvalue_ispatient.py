d8a142f92f494842aa46841ddcdcab573dbae2df
root <root@smallbrain-dev.c.smallbrain-dev.internal>
2016-11-18 22:42:50 +0000

    pushing migration 0106 to repo for migration made on server.

---
+++ apps/emr/migrations/0106_inrvalue_ispatient.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('emr', '0105_auto_20161107_0102'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='inrvalue',
+            name='ispatient',
+            field=models.BooleanField(default=False),
+        ),
+    ]
