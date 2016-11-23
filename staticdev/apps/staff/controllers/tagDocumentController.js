922d4828f28ca9ae9bfa4830b9da1e8f37fd3f8f
anhdn <ngocanh221291@gmail.com>
2016-11-23 14:40:23 +0700

    Add loading active todo & problem in document tagging page

--- staticdev/apps/staff/controllers/tagDocumentController.js
+++ staticdev/apps/staff/controllers/tagDocumentController.js
@@ -3,22 +3,35 @@
     angular.module('StaffApp')
         .controller('TagDocumentCtrl', TagDocumentCtrl);
 
-    TagDocumentCtrl.$inject = ['$scope', 'documentService', '$routeParams'];
+    TagDocumentCtrl.$inject = ['$scope', 'documentService', '$routeParams', 'staffService'];
 
     /**
      *
      * @param $scope
      * @param documentService
      * @param $routeParams
+     * @param staffService
      * @constructor
      */
-    function TagDocumentCtrl($scope, documentService, $routeParams) {
+    function TagDocumentCtrl($scope, documentService, $routeParams, staffService) {
 
         documentService.getDocumentInfo($routeParams.documentId).then(function (resp) {
             $scope.document = resp.data.info;
+            if (resp.data.info.patient != null) {
+                // Fetch user's probs
+                var patientId = resp.data.info.patient.id;
+                staffService.fetchProblems(patientId).then(function (response) {
+                    $scope.active_probs = response.problems;
+                });
+                // Fetch user's todos
+                staffService.fetchPatientTodos(patientId).then(function (data) {
+                    $scope.active_todos = data['pending_todos']; // aka active todo
+                });
+            }
         });
 
         $scope.pinDocument2Patient = pinDocument2Patient;
+
         $scope.pinDocument2Todo = documentService.pinDocument2Todo;
         $scope.pinDocument2Problem = documentService.pinDocument2Problem;
         function pinDocument2Patient() {
