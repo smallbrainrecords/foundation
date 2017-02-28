0636c0b7c9135f9ce0b296f3e26157df70137c5d
anhdn <ngocanh221291@gmail.com>
2017-02-28 09:48:38 +0700

    - Added SMTP for report production internal server error

--- project/settings.py
+++ project/settings.py
@@ -239,3 +239,12 @@ try:
     from local_settings import *
 except ImportError as e:
     pass
+
+
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_PASSWORD = 'anhdan221291' #my gmail password
+EMAIL_HOST_USER = 'chiase.tmp@gmail.com' #my gmail username
+EMAIL_PORT = 587
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
