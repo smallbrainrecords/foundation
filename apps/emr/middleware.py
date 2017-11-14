c8ea2b53ee540031af20cbc2c9a1643435018ffc
Kevin Perdue <root@smallbrain-dev.c.smallbrain-dev.internal>
2017-11-14 03:09:45 +0000

    Filter out /encounter/status checks from accesslog

--- apps/emr/middleware.py
+++ apps/emr/middleware.py
@@ -5,6 +5,6 @@ from .models import AccessLog
 class AccessLogMiddleware(object):
 
     def process_request(self, request):
-        if request.user.is_authenticated() and not request.path.startswith('/list_of'):
+        if request.user.is_authenticated() and not request.path.startswith('/list_of') and not request.path.endswith('/encounter/status'):
             access_log = AccessLog(user=request.user, summary=request.path)
             access_log.save()
