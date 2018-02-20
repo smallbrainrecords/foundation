6ad2d05e694e16e960612f9afa12db0df140a448
anhdn <ngocanh221291@gmail.com>
2018-02-20 17:54:02 +0700

    - Fixed issues ANDROMEDA-HEALTH-BE-14 (AttributeError: type object 'datetime.datetime' has no attribute 'datetime') while create new user

--- apps/project_admin_app/views.py
+++ apps/project_admin_app/views.py
@@ -14,7 +14,6 @@ GNU Affero General Public License for more details.
 You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 """
-import datetime
 import logging
 
 from django.contrib.auth.decorators import login_required
@@ -315,8 +314,6 @@ def create_user(request):
                     new_user.save()
                 except Exception as e:
                     raise e
-                    logging.error(e)
-
                     new_user = None
 
                 if new_user is not None:
