ce74fb932cfff43bb0eb4ff3bfaaeb6ff92423bf
michikevlar <root@smallbrain-dev.c.smallbrain-dev.internal>
2018-03-15 19:24:58 +0000

    update to cron.sh path settings
    clean-up edit patient profile page

--- apps/emr/cron.sh
+++ apps/emr/cron.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-cd /home/softdevelop_vd
+cd /var/www/andromeda/foundation
 source /home/softdevelop_vd/Env/foundation/bin/activate
-cd foundation
+
 python manage.py cron review_colorectal_cancer_risk_assessment
 python manage.py cron patient_needs_a_plan_for_colorectal_cancer_screening
 python manage.py cron a1c_order_was_automatically_generated
