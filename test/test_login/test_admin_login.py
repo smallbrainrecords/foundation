552953598e1c33260769fd9c91fcd9cb9e0bb248
Jeison Araya Mena <jeison.arayamena@ucr.ac.cr>
2021-08-30 23:00:02 -0600

    login tests added

---
+++ test/test_login/test_admin_login.py
@@ -0,0 +1,63 @@
+"""Test login as admin"""
+from test.common import build_driver, login, load_data, ADMIN_USER
+from django.test import LiveServerTestCase
+from emr.models import User, UserProfile
+
+
+class TestAdminLogin(LiveServerTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        """
+        Prepare environment before run the tests
+        """
+        super(TestAdminLogin, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Clean up the environment and db after all tests have finished.
+        """
+        super(TestAdminLogin, cls).tearDownClass()
+
+    def test_admin_login(self):
+        """
+        Test login as admin with correct credentials.
+        """
+        try:
+            # Prepare test
+            load_data()
+            driver = build_driver()
+
+            # Login as admin
+            login(driver, 
+                base_url=self.live_server_url, 
+                username=ADMIN_USER['username'], 
+                password=ADMIN_USER['password'])
+
+            # Check results
+            assert driver.current_url == '{}/project/{}/#/'.format(
+                self.live_server_url, ADMIN_USER['username']), 'Login failed: user -> {}, {}'.format(ADMIN_USER['username'], ADMIN_USER['password'])
+        finally:
+            driver.quit()
+
+    def test_admin_login_with_incorrect_password(self):
+        """
+        Test login as admin with an incorrect password.
+        """
+        try:
+            # Prepare test
+            load_data()
+            driver = build_driver()
+            
+            # Login as admin
+            login(driver,
+                base_url=self.live_server_url, 
+                username=ADMIN_USER['username'], 
+                password=ADMIN_USER['password'] + 'xyz')
+
+            assert driver.current_url == '{}/u/login/'.format(
+                self.live_server_url), 'Login with incorrent password failed: user -> {}'.format(ADMIN_USER['username'])
+        finally:
+            driver.quit()
+
---
+++ test/test_login/test_patient_login.py
@@ -0,0 +1,62 @@
+"""Test login as patient"""
+from test.common import build_driver, login, load_data, PATIENT_USER
+from django.test import LiveServerTestCase
+from emr.models import User, UserProfile
+
+class TestPatientLogin(LiveServerTestCase):
+    """
+    Login test as patient user.
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+        Prepare environment before run the tests
+        """
+        super(TestPatientLogin, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Clean up the environment and db after all tests have finished.
+        """
+        super(TestPatientLogin, cls).tearDownClass()
+
+    def test_patient_login(self):
+        """
+        Test login as patient user with correct credentials.
+        """
+        try:
+            # Prepare test
+            load_data()
+            driver = build_driver()
+            
+            # Login as patient
+            login(driver, 
+                base_url=self.live_server_url,
+                username=PATIENT_USER['email'], 
+                password=PATIENT_USER['password'])
+
+            assert str(driver.current_url).startswith('{}/u/patient/manage/'.format(
+                self.live_server_url)), 'Login failed: user -> {}, {}'.format(PATIENT_USER['username'], PATIENT_USER['password'])
+        finally:
+            driver.quit()
+
+    def test_patient_login_with_incorrect_password(self):
+        """
+        Test login as patient user with incorrect password.
+        """
+        try:
+            # Prepare test
+            load_data()
+            driver = build_driver()
+
+            # Login test
+            login(driver, 
+                base_url=self.live_server_url,
+                username=PATIENT_USER['email'], 
+                password=PATIENT_USER['password'] + 'xyz')
+
+            assert driver.current_url == '{}/u/login/'.format(
+                self.live_server_url), 'Login with incorrent password failed: user -> {}'.format(PATIENT_USER['username'])
+        finally:
+            driver.quit()
