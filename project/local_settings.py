f5028e9a22d2ff9ab44003b16c102b1004e4d711
Jeison Araya Mena <jeison.arayamena@ucr.ac.cr>
2021-08-30 22:52:45 -0600

    test database name added

--- project/local_settings.py
+++ project/local_settings.py
@@ -1,44 +1,41 @@
 import os
 
-BASE_DIR  = os.getcwd()
-
-
+BASE_DIR = os.getcwd()
 
 PROBLEMS_PATH = os.path.join(BASE_DIR, 'static/js/problems')
 
+COMPRESS_ENABLED = True
 
-COMPRESS_ENABLED = False
+SECRET_KEY = 'secret-key'
 
+ALLOWED_HOSTS = ['127.0.0.1']
 
 SNOMEDCT = {
-        'snomedct': {
-            'ssl': {'key':'/path/to/key','cert':'/path/to/cert','ca':'/path/to/ca'},
-            'host':'host',
-            'user':'user',
-            'passwd':'passwd',
-            'db':'snomedct',
-         }
+    'snomedct': {
+        'ssl': {'key': '/path/to/key', 'cert': '/path/to/cert', 'ca': '/path/to/ca'},
+        'host': 'host',
+        'user': 'user',
+        'passwd': 'passwd',
+        'db': 'snomedct',
+    }
 }
 
 DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'NAME',                      # Or path to database file if using sqlite3.
-            'USER': 'USER',                      # Not used with sqlite3.
-            'PASSWORD': 'PASSWORD',                  # Not used with sqlite3.
-            'HOST': 'HOST',                      # Set to empty string for localhost. Not used with sqlite3.
-            'PORT': 'PORT',                      # Set to empty string for default. Not used with sqlite3.
-            'OPTIONS': {'ssl':{'key':'/path/to/key','cert':'/path/to/cert','ca':'/path/to/ca'}},
-        }
-    }
+    'default': {
+        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.mysql',
+        # Or path to database file if using sqlite3.
+        'NAME': 'test',
+        'USER': 'root',                      # Not used with sqlite3.
+        'PASSWORD': 'root',                  # Not used with sqlite3.
+        # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',
+        # Set to empty string for default. Not used with sqlite3.
+        'PORT': '3306',
+        'OPTIONS': {},
+        'TEST': {
+            'NAME': 'test_test',
+        },
 
-# DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-#             'NAME': 'sbtest',                      # Or path to database file if using sqlite3.
-#             'USER': 'root',                      # Not used with sqlite3.
-#             'PASSWORD': 'Volatile0',                  # Not used with sqlite3.
-#             'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-#             'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-#         }
-#     }
+    }
+}
