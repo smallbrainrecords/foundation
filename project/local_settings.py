37a6e3b33697e7d3eb6297d23b554fb89bc3318c
Jeison Araya Mena <jeison.arayamena@ucr.ac.cr>
2021-10-12 15:21:49 -0600

    Environment variables settings added

--- project/local_settings.py
+++ project/local_settings.py
@@ -1,12 +1,14 @@
 import os
 
+from decouple import config
+
 BASE_DIR = os.getcwd()
 
 PROBLEMS_PATH = os.path.join(BASE_DIR, 'static/js/problems')
 
 COMPRESS_ENABLED = True
 
-SECRET_KEY = 'secret-key'
+SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
@@ -25,16 +27,16 @@ DATABASES = {
         # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.mysql',
         # Or path to database file if using sqlite3.
-        'NAME': 'test',
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'root',                  # Not used with sqlite3.
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),                      # Not used with sqlite3.
+        'PASSWORD': config('DATABASE_PASSWORD'),                  # Not used with sqlite3.
         # Set to empty string for localhost. Not used with sqlite3.
-        'HOST': '127.0.0.1',
+        'HOST': config('DATABASE_HOST'),
         # Set to empty string for default. Not used with sqlite3.
-        'PORT': '3306',
+        'PORT': config('DATABASE_PORT'),
         'OPTIONS': {},
         'TEST': {
-            'NAME': 'test_test',
+            'NAME': config('DATABASE_TEST_NAME'),
         },
 
     }
