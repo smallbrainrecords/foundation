16ce3b28a4020074af826f7b165c6dae516d2b22
Ricardo Franco <rfrancoch@gmail.com>
2022-05-16 09:24:39 -0600

    use os env variables

--- project/local_settings.py
+++ project/local_settings.py
@@ -1,7 +1,8 @@
 from google.oauth2 import service_account
 import os
+from dotenv import load_dotenv
 
-from decouple import config
+load_dotenv()
 
 BASE_DIR = os.getcwd()
 
@@ -9,7 +10,7 @@ PROBLEMS_PATH = os.path.join(BASE_DIR, 'static/js/problems')
 
 COMPRESS_ENABLED = True
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = ['*']
 
@@ -25,21 +26,15 @@ SNOMEDCT = {
 
 DATABASES = {
     'default': {
-        # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'ENGINE': 'django.db.backends.mysql',
-        # Or path to database file if using sqlite3.
-        'NAME': config('DATABASE_NAME'),
-        # Not used with sqlite3.
-        'USER': config('DATABASE_USER'),
-        # Not used with sqlite3.
-        'PASSWORD': config('DATABASE_PASSWORD'),
-        # Set to empty string for localhost. Not used with sqlite3.
-        'HOST': config('DATABASE_HOST'),
-        # Set to empty string for default. Not used with sqlite3.
-        'PORT': config('DATABASE_PORT'),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
         'OPTIONS': {},
         'TEST': {
-            'NAME': config('DATABASE_TEST_NAME'),
+            'NAME': os.getenv('DATABASE_TEST_NAME'),
         },
 
     }
@@ -47,19 +42,19 @@ DATABASES = {
 
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
-GS_BUCKET_NAME = config('GCP_BUCKET_NAME')
+GS_BUCKET_NAME = os.getenv('GCP_BUCKET_NAME')
 
 gcp_sa_credentials = {
     "type": "service_account",
-    "project_id": config('GCP_PROJECT_ID'),
-    "private_key_id": config('GCP_PRIVATE_KEY_ID'),
-    "private_key": config('GCP_PRIVATE_KEY').replace('\\n', '\n'),
-    "client_email": config('GCP_CLIENT_EMAIL'),
-    "client_id": config('GCP_CLIENT_ID'),
+    "project_id": os.getenv('GCP_PROJECT_ID'),
+    "private_key_id": os.getenv('GCP_PRIVATE_KEY_ID'),
+    "private_key": os.getenv('GCP_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": os.getenv('GCP_CLIENT_EMAIL'),
+    "client_id": os.getenv('GCP_CLIENT_ID'),
     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
     "token_uri": "https://oauth2.googleapis.com/token",
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-    "client_x509_cert_url": config('GCP_CLIENT_X509_CERT_URL')
+    "client_x509_cert_url": os.getenv('GCP_CLIENT_X509_CERT_URL')
 }
 
 GS_CREDENTIALS = service_account.Credentials.from_service_account_info(
