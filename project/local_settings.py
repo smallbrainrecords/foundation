022ef398af93ec8a1dcdf899f21aecaeb65a8608
Ricardo Franco <rfrancoch@gmail.com>
2022-06-23 16:25:19 -0600

    Adding envs to connect to the DB using ceritificates

--- project/local_settings.py
+++ project/local_settings.py
@@ -47,6 +47,43 @@ DATABASES = {
     }
 }
 
+if os.getenv('DATABASE_USE_SSL').lower() == "true":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DATABASE_NAME'),
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': os.getenv('DATABASE_HOST'),
+            'PORT': os.getenv('DATABASE_PORT'),
+            'OPTIONS': {
+                'ssl': {
+                    'ca': os.getenv('DATABASE_SSL_CA'),
+                    'cert': os.getenv('DATABASE_SSL_CERT'),
+                    'key': os.getenv('DATABASE_SSL_KEY'),
+                }
+            },
+            'TEST': {
+                'NAME': os.getenv('DATABASE_TEST_NAME'),
+            },
+        },
+        'snomedict': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.getenv('DATABASE_NAME_SNOMEDCT'),
+            'USER': os.getenv('DATABASE_USER'),
+            'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+            'HOST': os.getenv('DATABASE_HOST'),
+            'PORT': os.getenv('DATABASE_PORT'),
+            'OPTIONS': {
+                'ssl': {
+                    'ca': os.getenv('DATABASE_SSL_CA'),
+                    'cert': os.getenv('DATABASE_SSL_CERT'),
+                    'key': os.getenv('DATABASE_SSL_KEY'),
+                }
+            },
+        }
+    }
+
 DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 STATICFILES_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = os.getenv('GCP_BUCKET_NAME')
